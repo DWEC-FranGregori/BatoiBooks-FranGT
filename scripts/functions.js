3210a23ab189bfeae97f871015e10739e61a604f
Fran Gregori Tandazo <frangregori61@gmail.com>
2023-09-28 15:48:14 +0200

    [DONE] booksWithStatus method

--- scripts/functions.js
+++ scripts/functions.js
@@ -10,4 +10,7 @@ function booksCheeperThan(array, number) {
   return array.filter((book) => book.price <= number);
 }
 
+function booksWithStatus(array, string) {
+  return array.filter((book) => book.status === string);
+}
 export default { booksFromUser, booksFromModule, booksCheeperThan };
