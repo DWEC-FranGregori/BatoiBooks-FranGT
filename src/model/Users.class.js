1dca9bd4a6e440d83f6d191f924218ccea11fbc6
Fran Gregori Tandazo <frangregori61@gmail.com>
2023-10-04 16:06:46 +0200

    Users done

--- src/model/Users.class.js
+++ src/model/Users.class.js
@@ -4,21 +4,24 @@ class Users extends UsersInterface {
   constructor() {
     this.data = [];
   }
+
   populateData(arrayUsers) {
     arrayUsers.forEach((user) => {
       this.data.push(new User(user.id, user.email, user.nickname));
     });
   }
+
   addItem(user) {
     user.id = getNewId();
     this.data.push(user);
   }
+
   removeItem(id) {
     const userToRemove = this.getItemById(id);
-    if(userToRemove == {}){
+    if (userToRemove == {}) {
       throw new IdNotFound();
     }
-    this.data= this.data.filter(function(user){
+    this.data = this.data.filter(function (user) {
       return user.id !== id;
     });
   }
@@ -78,11 +81,16 @@ function checkIsUndefined(data) {
   return data;
 }
 
-function getNewId(){
+function getNewId() {
   if (this.data.length < 1) {
     return 1;
   }
-  return ++this.data.reduce((userBefore, userAfter) => Math.max(userBefore.id, userAfter.id), -Infinity); 
+  return (
+    this.data.reduce(
+      (userBefore, userAfter) => Math.max(userBefore.id, userAfter.id),
+      -Infinity
+    ) + 1
+  );
 }
 
 module.exports = Users;
