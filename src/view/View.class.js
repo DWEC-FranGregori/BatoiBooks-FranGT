e817b315cabf683d1d308e2a5dbe710b598cf728
Fran Gregori Tandazo <frangregori61@gmail.com>
2023-11-02 16:52:00 +0100

    quitado el toFixed

--- src/view/View.class.js
+++ src/view/View.class.js
@@ -1,69 +1,71 @@
-export default class View{
+export default class View {
   constructor() {
-    this.list = document.getElementById('list')
-    this.about = document.getElementById('about')
-    this.form = document.getElementById('form')
-    this.remove = document.getElementById('remove')
-    this.bookForm = document.getElementById('bookForm')
-    this.messages = document.getElementById('messages')
+    this.list = document.getElementById("list");
+    this.about = document.getElementById("about");
+    this.form = document.getElementById("form");
+    this.remove = document.getElementById("remove");
+    this.bookForm = document.getElementById("bookForm");
+    this.messages = document.getElementById("messages");
   }
 
   renderBook(book) {
-    const bookUI = document.createElement('div')
-    bookUI.id = 'book-'+book.id
-    bookUI.className = 'card'
+    const bookUI = document.createElement("div");
+    bookUI.id = "book-" + book.id;
+    bookUI.className = "card";
     bookUI.innerHTML = `
     <img src="${book.photo}" alt="Lbro: ${book.id}">
       <div>
-        <h5>${book.idModule + ' (' + book.id +')'}</h5>
+        <h5>${book.idModule + " (" + book.id + ")"}</h5>
         <h6>${book.publisher}</h6>
-        <p>Precio: ${book.price.toFixed(2)} €</p>
+        <p>Precio: ${book.price} €</p>
         <p>Páginas: ${book.pages}</p>
         <p>Estado: ${book.status}</p>
-        <p>${book.soldDate?'Vendido el ' + book.soldDate:'En venta'}</p>
-        <p>Comentarios: ${book.comments || ''}</p>
+        <p>${book.soldDate ? "Vendido el " + book.soldDate : "En venta"}</p>
+        <p>Comentarios: ${book.comments || ""}</p>
       </div>
-    `
-    this.list.appendChild(bookUI)
-    this.bookForm.reset()
+    `;
+    this.list.appendChild(bookUI);
+    this.bookForm.reset();
   }
 
   renderModulesInSelect(modules) {
-    const selectModules = document.getElementById('id-module')
+    const selectModules = document.getElementById("id-module");
     modules.forEach((module) => {
-        const moduleUI = document.createElement('option')
-        moduleUI.value = module.code
-        moduleUI.textContent = module.cliteral
-        selectModules.appendChild(moduleUI)
-    })
+      const moduleUI = document.createElement("option");
+      moduleUI.value = module.code;
+      moduleUI.textContent = module.cliteral;
+      selectModules.appendChild(moduleUI);
+    });
   }
 
   renderRemoveBook(id) {
-    const bookUI = document.getElementById('book-'+id)
-    bookUI.parentElement.removeChild(bookUI)
+    const bookUI = document.getElementById("book-" + id);
+    bookUI.parentElement.removeChild(bookUI);
   }
 
   renderErrorMessage(type, message) {
-    const messageUI = document.createElement('div')
-    messageUI.className = type + ' alert alert-danger alert-dismissible'
-    messageUI.setAttribute('role', 'alert')
+    const messageUI = document.createElement("div");
+    messageUI.className = type + " alert alert-danger alert-dismissible";
+    messageUI.setAttribute("role", "alert");
     messageUI.innerHTML = `
     ${message}
     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close" onclick="this.parentElement.remove()">x</button>
-    `
-    this.messages.appendChild(messageUI)
-    window.scroll(0,0)
+    `;
+    this.messages.appendChild(messageUI);
+    window.scroll(0, 0);
   }
 
   getBookFormValues() {
-    const idModule = this.bookForm.elements['id-module'].value
+    const idModule = this.bookForm.elements["id-module"].value;
     // También podríamos coger el input directamente con su id
     // document.getElementById('id-module').value
-    const publisher = this.bookForm.elements.publisher.value
-    const price = this.bookForm.elements.price.value
-    const pages = this.bookForm.elements.pages.value
-    const status = this.bookForm.querySelector('input[name="status"]:checked').value
-    const comments = this.bookForm.elements.comments.value
+    const publisher = this.bookForm.elements.publisher.value;
+    const price = this.bookForm.elements.price.value;
+    const pages = this.bookForm.elements.pages.value;
+    const status = this.bookForm.querySelector(
+      'input[name="status"]:checked'
+    ).value;
+    const comments = this.bookForm.elements.comments.value;
 
     return {
       idModule,
@@ -71,8 +73,7 @@ export default class View{
       price,
       pages,
       status,
-      comments
-    }
+      comments,
+    };
   }
 }
-
